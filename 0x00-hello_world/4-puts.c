308239a7c3fa4cc95c58fa45e9df0607893aceb2
Irene-AA <alloteyirene05@gmail.com>
2022-04-09 22:47:26 -0700

    Added Quote

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 { 
-	puts("Programming is like building a multilinguual puzzle");
+	puts("\"Programming is like building a multilinguual puzzle\n");
 	return (0);
 }

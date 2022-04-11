0eb58a8565859786a8bc07360c4ba121d05fe8c4
Irene-AA <alloteyirene05@gmail.com>
2022-04-10 22:16:23 -0700

    Added comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - no arguments
+ *
+ * prints to the screen
+ * returns 0
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

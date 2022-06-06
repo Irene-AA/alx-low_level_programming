0e9225dd3db57e78d424d1f26917281c7baf132a
Irene-AA <alloteyirene05@gmail.com>
2022-06-05 20:23:38 -0700

    more c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main- main function
+ * Description: prints possible combination of 2 digits
+ * first digit smaller than the second
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+	int c;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			if (d < c)
+			{
+				putchar(d);
+				putchar(c);
+				if ( d != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

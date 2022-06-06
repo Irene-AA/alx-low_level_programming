62e70ac3930f5b436505b6f0343ce8325cc10bf0
a.irene <56001006+Irene-AA@users.noreply.github.com>
2022-06-05 17:44:29 -1200

    Update and rename 101-print_comb4 to 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main- main function
+ * Description: prints possible combination of 3 digits
+ * first digit smaller than the second and third
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+	int c;
+  	int e;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			for (e = '0' e <= '9'; e++)
+			{
+				if (d < c && d < e)
+				{
+					putchar(d);
+					putchar(c);
+					putchar(e);
+						if ( d != '7')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

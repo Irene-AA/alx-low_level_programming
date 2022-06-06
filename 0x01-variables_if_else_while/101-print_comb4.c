5e03f07a67d875e8245fa3a7ad0a2547ce27bcc9
Irene-AA <alloteyirene05@gmail.com>
2022-06-05 22:51:53 -0700

    Betty

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,24 @@ int main(void)
 {
 	int d;
 	int c;
-  	int e;
+	int e;
 
 	for (d = '0'; d <= '9'; d++)
 	{
 		for (c = '0'; c <= '9'; c++)
 		{
-			for (e = '0' e <= '9'; e++)
+			for (e = '0'; e <= '9'; e++)
 			{
 				if (d < c && d < e)
 				{
 					putchar(d);
 					putchar(c);
 					putchar(e);
-						if ( d != '7')
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (d != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}

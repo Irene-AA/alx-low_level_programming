3031c441affa4974d50b0077ea22c0817ff60621
a.irene <56001006+Irene-AA@users.noreply.github.com>
2022-06-14 15:21:33 -1200

    reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - void function
+ * @s: string 
+ *Description - reverses a string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int c, d = 0;
+	char e[];
+
+	while (*(s + c))
+	{
+		*(e + c) = *(s + c);
+		c++;
+	}
+	c = c - 1;
+	while (c >= 0)
+	{
+		*(s + c) = *(e + d);
+		d++;
+		c--;
+	}
+}

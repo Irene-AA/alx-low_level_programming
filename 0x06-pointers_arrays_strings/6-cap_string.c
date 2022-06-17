420122dbbeac420e1dbf606ea012ea146df1ead3
a.irene <56001006+Irene-AA@users.noreply.github.com>
2022-06-16 14:43:37 -1200

    capitalizes words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * cap_string - capitalizes all words in string
+  * @s: string
+  * Return: string
+  */
+char *cap_string(char *s)
+{
+	int a = 0, b;
+	int caps = 13;
+	char sp[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (i < caps)
+		{
+			if ((a == 0 || s[a - 1] == sp[b]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}

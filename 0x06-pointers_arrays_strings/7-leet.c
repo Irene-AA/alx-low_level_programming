6a3dfe33bf1932476751afca794b7fd49cb6214c
Irene-AA <alloteyirene05@gmail.com>
2022-06-16 21:27:58 -0700

    encodes strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,14 +7,15 @@
   */
 char *leet(char *s)
 {
-	int a = 0, e = 0;
+	int a = 0, e, j = 5;
 	char b[5] = {'A', 'E', 'O', 'T', 'L'};
 	char c[5] = {'4', '3', '0', '7', '1'};
 
 	while (s[a])
 	{
-		while (a < 5)
+		while (a < j)
 		{
+			e = 0;
 			if (s[a] == b[e] || s[a] - 32 == b[e])
 			{
 				s[a] = c[e];

8e8049b5205e1274c07c2ff3341fa0a415bb5adf
a.irene <56001006+Irene-AA@users.noreply.github.com>
2022-06-16 14:19:18 -1200

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+
+/**
+ * _strcat - concatenate two strings with no terminating null and n bytes
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+
+	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
+	{
+		dest[a] = src[b];
+	}
+	return (dest);
+}

64be824c949f6f78ccdd50e504c6b3349ba93a21
Irene-AA <alloteyirene05@gmail.com>
2022-06-29 00:29:09 +0000

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main: main function with arguments
+ * Description: prints program name
+ * @argc: argument size
+ * @argv: augument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	printf("%s\n", argv[0]);
+	return (0);
+}

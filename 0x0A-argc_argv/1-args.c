9912e03fc5bffc1b425cc9d1630ca124d0e759f1
Irene-AA <alloteyirene05@gmail.com>
2022-06-29 17:47:47 +0000

    prints argument number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,15 @@
 /**
  * main: main function
  * Description- prints number of arguments passed
- *@argc: argument size
- @argv: argument vector
+ * @argc: argument size
+ * @argv: argument vector
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	void(argv)
+	(void) argv;
+
 	printf("%d\n", argc -1);
 	return (0);
 }

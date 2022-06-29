48fd7acdfa9b032ddb1c3c2dc381f74bc0098fab
Irene-AA <alloteyirene05@gmail.com>
2022-06-29 17:28:44 +0000

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	t = malloc(sizeof(*t) * 30);
+	t = malloc(sizeof(*t) * size);
 	for (i = 0; i < size; i++)
 	{
 		t[i] = c;

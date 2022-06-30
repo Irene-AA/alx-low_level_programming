2099e4dc60b20a7c53b01637e0f86503ab7c2143
Irene-AA <alloteyirene05@gmail.com>
2022-06-30 16:24:47 +0000

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
- * main- copies string to a new allocated memory
+ * _strdup- copies string to a new allocated memory
  * @str: string
  * Return: Pointer or NULL
  */
@@ -19,8 +19,6 @@ char *_strdup(char *str)
 	for (i = 0; i < strlen(str); i++)
 	{
 		d[i] = str[i];
-		/*if (sizeof(d) > sizeof(char))
-			return (NULL);**/
 	}
 	return (d);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 /**
- * main - concatenates 2 strings
+ * str_concat - concatenates 2 strings
  * @s1: string 1
  * @s2: string 2
  * Return: result or NULL
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;
 
 	t = malloc(sizeof(s1) * sizeof(s2) + 1);
-	for (i = 0;  ; i++)
+	for (i = 0; i < strlen(s2); i++)
 	{
 		if (i < strlen(s1))
 			t[i] = s1[i];

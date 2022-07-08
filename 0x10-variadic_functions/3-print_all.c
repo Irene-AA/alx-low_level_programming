5ef600a6000e2253073daf91d31ce37af4f80eb0
a.irene <56001006+Irene-AA@users.noreply.github.com>
2022-07-07 16:15:39 -1200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,11 +11,68 @@ void printchar(va_list a)
 }
 
 /**
- * print_all - prints anything
- * @format: argument types
+ * printstr - prints string type
+ * @list: va_list passed
  */
-
-void print_all(const char *const format, ...)
+void printstr(va_list a)
 {
-	va_list a;
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * printfloat - prints float type
+ * @list: va_list passed
+ */
+void printfloat(va_list a)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * printint - prints int type
+ * @list: va_list passed
+ */
+void printint(va_list a)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_all - prints anything passed if char, int, float, or string.
+ * @format: string of formats
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list b;
+	char *sp;
+
+	checker storage[] = {
+		{ "c", printchar },
+		{ "f", printfloat },
+		{ "s", printstr },
+		{ "i", printint }
+	};
+
+	i = 0;
+	sp = "";
+	va_start(b, format);
+	while (format != NULL && format[i / 4] != '\0')
+	{
+		j = i % 4;
+		if (storage[j].type[0] == format[i / 4])
+		{
+			printf("%s", sp);
+			storage[j].f(b);
+			sp = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(b);
 }
